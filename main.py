245f74da950c1e9d24c13434e95847802c911060
NasrulKarib <karibarian12@gmail.com>
2025-11-02 14:59:56 +0600

    formatting the main.py

--- main.py
+++ main.py
@@ -1,39 +1,37 @@
-from fastapi import FastAPI, Path, HTTPException, Query
+import json
+from typing import Annotated, Literal, Optional
+
+from fastapi import FastAPI, HTTPException, Query
 from fastapi.responses import JSONResponse
 from pydantic import BaseModel, Field, computed_field
-from typing import Annotated, Literal, Optional
-import json
 
 app = FastAPI()
 
 class Patient(BaseModel):
-     
-     id: Annotated[str, Field(..., description='ID of the patient', examples=['P001'])]
-     name: Annotated[str, Field(..., description='Name of the patient')]
-     city: Annotated[str, Field(..., description='City where the patient is living')]
-     age: Annotated[int, Field(..., gt=0, lt=120, description='Age of the patient')]
-     gender: Annotated[Literal['male', 'female', 'others'], Field(..., description='Gender of the patient')]
-     height: Annotated[float, Field(..., gt=0, description='Height of the patient in mtrs')]
-     weight: Annotated[float, Field(..., gt=0, description='Weight of the patient in kgs')]
+    id: Annotated[str, Field(..., description='ID of the patient', examples=['P001'])]
+    name: Annotated[str, Field(..., description='Name of the patient')]
+    city: Annotated[str, Field(..., description='City where the patient is living')]
+    age: Annotated[int, Field(..., gt=0, lt=120, description='Age of the patient')]
+    gender: Annotated[Literal['male', 'female', 'others'], Field(..., description='Gender of the patient')]
+    height: Annotated[float, Field(..., gt=0, description='Height of the patient in mtrs')]
+    weight: Annotated[float, Field(..., gt=0, description='Weight of the patient in kgs')]
 
-     @computed_field
-     @property
-     def bmi(self) -> float:
-        bmi = round(self.weight/(self.height**2),2)
-        return bmi
+    @computed_field
+    @property
+    def bmi(self) -> float:
+       bmi = round(self.weight/(self.height**2),2)
+       return bmi
     
-     @computed_field
-     @property
-     def verdict(self) -> str:
-
+    @computed_field
+    @property
+    def verdict(self) -> str:    
         if self.bmi < 18.5:
-            return 'Underweight'
-        elif self.bmi < 25:
-            return 'Normal'
-        elif self.bmi < 30:
-            return 'Normal'
-        else:
-            return 'Obese'
+           return 'Underweight'
+        if self.bmi < 25:
+           return 'Normal'
+        if self.bmi < 30:
+           return 'Normal'
+        return 'Obese'
 
 class PatientUpdate(BaseModel):
     name: Annotated[Optional[str], Field(default=None)]
@@ -44,15 +42,14 @@ class PatientUpdate(BaseModel):
     weight: Annotated[Optional[float], Field(default=None, gt=0)]
 
 def load_data():
-    with open('patients.json', 'r') as f:
+    with open('patients.json', 'r', encoding='utf-8') as f:
         data = json.load(f)
 
     return data
 
 def save_data(data):
-    with open('patients.json', 'w') as f:
-        json.dump(data, f)
-        
+    with open('patients.json', 'w', encoding='utf-8') as f:
+        json.dump(data, f)       
 
 @app.get("/")
 def hello():
@@ -77,8 +74,7 @@ def view_patient(patient_id: str):
     raise HTTPException(status_code=404, detail='Patient not found')
 
 @app.get('/sort')
-def sort_patients(sort_by: str = Query(..., description='Sort on the basis of height, weight or bmi'), order: str = Query('asc', description='sort in asc or desc order')):
-
+def sort_patients(sort_by: str = Query(..., description='Basis of height, weight or bmi'), order: str = Query('asc', description='asc or desc order')):
     valid_fields = ['height', 'weight', 'bmi']
 
     if sort_by not in valid_fields:
@@ -86,13 +82,9 @@ def sort_patients(sort_by: str = Query(..., description='Sort on the basis of he
     
     if order not in ['asc', 'desc']:
         raise HTTPException(status_code=400, detail='Invalid order select between asc and desc')
-    
     data = load_data()
-
-    sort_order = True if order=='desc' else False
-
+    sort_order = order == 'desc' 
     sorted_data = sorted(data.values(), key=lambda x: x.get(sort_by, 0), reverse=sort_order)
-
     return sorted_data
 
 @app.post('/create')
@@ -103,21 +95,18 @@ def create_patient(patient: Patient):
         return HTTPException(status_code = 400, detail = 'Patient already exists')
     
     data[patient.id] = patient.model_dump(exclude = 'id')
-
     save_data(data) 
-
+    
     return JSONResponse(status_code = 201, content = {"message": "Patient is created successfully"})
 
 @app.put('/edit/{patient_id}')
 def update_patient(patient_id: str, patient_update: PatientUpdate):
-
     data = load_data()
 
     if patient_id not in data:
         raise HTTPException(status_code=404, detail='Patient not found')
     
     existing_patient_info = data[patient_id]
-
     updated_patient_info = patient_update.model_dump(exclude_unset=True)
 
     for key, value in updated_patient_info.items():
@@ -125,15 +114,9 @@ def update_patient(patient_id: str, patient_update: PatientUpdate):
 
     existing_patient_info['id'] = patient_id
     patient_pydandic_obj = Patient(**existing_patient_info)
-  
     existing_patient_info = patient_pydandic_obj.model_dump(exclude='id')
-
-    
     data[patient_id] = existing_patient_info
-
-    
     save_data(data)
-
     return JSONResponse(status_code=200, content={'message':'patient updated'})
 
 @app.delete('/delete/{patient_id}')
