cf90c81d613dc7ddc7bbce5285105d38a4626d44
NasrulKarib <karibarian12@gmail.com>
2025-11-02 15:08:00 +0600

    add fail condition for pylint

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
 import json
 from typing import Annotated, Literal, Optional
-
 from fastapi import FastAPI, HTTPException, Query
 from fastapi.responses import JSONResponse
 from pydantic import BaseModel, Field, computed_field
